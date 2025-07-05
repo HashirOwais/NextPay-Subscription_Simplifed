b61328a7d586b70ceae0d86d4e39e97e472c3f2c
Muhammad Tariq <muhammad.etariq@gmail.com>
2025-07-05 16:02:26 -0600

    addsubscription test failing

--- nextpay/src/test/java/com/example/db_moduleTest.java
+++ nextpay/src/test/java/com/example/db_moduleTest.java
@@ -1,10 +1,18 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class db_moduleTest {
+    static db_module db_module;
+    @BeforeAll
+    static void setupDatabase(){
+        db_module = new db_module();
+        db_module.DBConnection();        
+    }
 
 
     @Test
@@ -15,7 +23,16 @@ public class db_moduleTest {
         assertTrue(demo);
 
     }
+    @Test
+    public void addSubscriptionTest() {
+        db_module db = new db_module();
+        Subscription s = new Subscription(0, "Spotify", 8.99, true, "Monthly", "2025-07-05", 1);
+        assertTrue(db.addSubscription(s)); 
+    }
+    //subID, name, cost, recurring, cycletype, cycledate, userid
 
+
+    //deleting subscription, add a thing first
 
     
 }
