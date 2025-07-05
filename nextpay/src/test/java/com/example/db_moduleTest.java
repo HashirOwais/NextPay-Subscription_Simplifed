5338d44deede561c65d35041fc30a2d160e8f4e0
Muhammad Tariq <muhammad.etariq@gmail.com>
2025-07-05 17:04:58 -0600

    pushed a negative case and some helper functions and other file changes to sync with team

--- nextpay/src/test/java/com/example/db_moduleTest.java
+++ nextpay/src/test/java/com/example/db_moduleTest.java
@@ -1,6 +1,9 @@
 package com.example;
 import com.example.models.Subscription;
+import java.time.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
@@ -18,23 +21,50 @@ public class db_moduleTest {
 
 
     @Test
-    public void test()
+    public void dbConnectionTest()
     {
         db_module db_module = new db_module();
         boolean demo = db_module.DBConnection();
         assertTrue(demo);
 
     }
+
+    //addSubscription: Positive Cases
     @Test
     public void addSubscription_ValidSubscription_True() {
         db_module db = new db_module();
         Subscription s = new Subscription(0, "Spotify", 8.99, true, "Monthly", LocalDate.parse("2025-07-05"), 1);
         assertTrue(db.addSubscription(s)); 
     }
-    //subID, name, cost, recurring, cycletype, cycledate, userid
-
-
-    //deleting subscription, add a thing first
+    @Test
+    public void addSubscription_ValidNonRecurringSubscription_True() 
+    {
+        db_module db = new db_module();
+        Subscription s = new Subscription(0, "Fortnite VBucks", 14.00, false, "Yearly", LocalDate.parse("2025-12-31"), 1);
+        assertTrue(db.addSubscription(s));
+    }
+    //addSubscription: Negative Cases
+    @Test
+    public void addSubscription_EmptyName_ReturnsFalse() {
+        db_module db = new db_module();
+        Subscription s = new Subscription(0, "", 8.99, true, "Monthly", LocalDate.parse("2025-07-05"), 1);
+        assertFalse(db.addSubscription(s));
+    }
 
     
+
+
+
+    @Test
+    public void findSubscriptionById_ValidId_ReturnsSubscription() {
+        Subscription s = db_module.findSubscriptionById(1);
+
+        assertNotNull(s); 
+    }
+
+    @Test
+    public void findSubscriptionById_InvalidId_ReturnsNull() {
+        Subscription s = db_module.findSubscriptionById(-1);
+        assertNull(s); 
+    }
 }
