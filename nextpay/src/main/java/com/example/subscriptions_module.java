37db81f0e7dc28e5767942b74298521fca213d44
Hashir Owais <Hashir Owais>
2025-07-16 18:11:13 -0600

    Completed made controller methods

--- nextpay/src/main/java/com/example/subscriptions_module.java
+++ nextpay/src/main/java/com/example/subscriptions_module.java
@@ -1,5 +1,25 @@
 package com.example;
 
+import com.example.models.Subscription;
+import java.util.List;
+import java.util.HashMap;
+
 public class subscriptions_module {
-    
+    private db_module db;
+
+    public subscriptions_module() {
+        db = new db_module();
+    }
+
+    // 1. View all subscriptions for a user
+    public List<Subscription> getAllSubscriptionsForUser(int userId) {
+        return db.viewSubscription(userId);
+    }
+
+    // 2. View monthly summary for a user
+    public String getMonthlySummaryString(int userId) {
+        HashMap<String, List<Subscription>> map = db.getMonthlySubscriptionSummary(userId);
+        // Always one entry, so grab the key (summary)
+        return map.keySet().iterator().next();
+    }
 }
